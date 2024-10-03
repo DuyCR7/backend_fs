38b48d901a462e9897e121e99bb6eebba91c6e1b
DuyCR7 <anhduy0317@gmail.com>
2024-10-03 23:41:02 +0700

    customer online

--- src/socket.js
+++ src/socket.js
@@ -6,16 +6,34 @@ const initSocketServer = (server) => {
         cors: "http://localhost:3000",  // CORS cho frontend
     });
 
+    let onlineCustomers = [];
+
     io.on('connection', (socket) => {
         console.log(`New client connected: ${socket.id}`);
 
-        socket.on('disconnect', () => {
-            console.log(`Client disconnected: ${socket.id}`);
-        });
+        socket.on("addNewCustomer", (cusId) => {
+            !onlineCustomers.some((cus) => cus.cusId === cusId) &&
+             onlineCustomers.push({ cusId, socketId: socket.id });
+             
+             console.log("======onlineCustomer", onlineCustomers);
+             io.emit('updateOnlineCustomers', onlineCustomers);
+        })
+
+        socket.on("disconnect", () => {
+            console.log("disconnect================================", socket.id)
+            onlineCustomers = onlineCustomers.filter(cus => cus.socketId !== socket.id);
+
+            console.log("======onlineCustomer", onlineCustomers);
+            io.emit('updateOnlineCustomers', onlineCustomers);
+        })
 
         socket.on('sendMessage', (message) => {
             console.log('Message received:', message);
             socket.broadcast.emit('receiveMessage', message);
+        });
+
+        socket.on('getOnlineCustomers', () => {
+            socket.emit('updateOnlineCustomers', onlineCustomers);
         });
     });
 
